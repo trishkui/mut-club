e36cc93296852622f0a77e26429c949140e83ab1
trishkui <graceirungu907@gmail.com>
2024-09-30 15:35:35 +0300

    fixed bugs

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,7 @@
 import React from "react"; 
 import Hero from "../../components/hero/hero";
 import About from "../../components/About/AboutUs";
-import Testimonies from "../../components/testimonies/testimonies";
+import Testimonies from "../../components/testimonies/Testimonies";
 import Header from "../../components/Header/header";
 
 function  Home() {
