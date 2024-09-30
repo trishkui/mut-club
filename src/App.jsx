20fe14843238ddb6c9b04694abef08b8bc06a949
trishkui <graceirungu907@gmail.com>
2024-09-30 15:22:25 +0300

    fixing bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Header from './components/Header/header';
 import Footer from './components/footer/footer';
 import Home from './pages/Home/home';
 import Leadership from './pages/Leadership/leadership';
-import Tracks from './pages/Tracks/tracks';
-import Events from './pages/Events/Events';
+import Tracks from './pages/Tracks/Tracks';
+import Events from './pages/events/events';
 import {BrowserRouter,Routes,Route } from 'react-router-dom'
 function App() {
  
